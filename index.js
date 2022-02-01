d498f89434d917226de8d08a437b5c0a413491de
Samuel-G3 <sgarciaine@gmail.com>
2022-02-01 12:27:31 +0100

    FEAT vercel cors2

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Headers", "Content-Type");
+  header('Access-Control-Allow-Origin: *');
   next();
 });
 
